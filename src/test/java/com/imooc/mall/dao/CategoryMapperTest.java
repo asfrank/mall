8afa082760666f42f2a59d0d8e4a46c220d1d9cd
lishuyu <lishuyu01@qiyi.com>
2020-02-01 15:08:42 +0800

    Initial commit

---
+++ src/test/java/com/imooc/mall/dao/CategoryMapperTest.java
@@ -0,0 +1,20 @@
+package com.imooc.mall.dao;
+
+import com.imooc.mall.MallApplicationTests;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CategoryMapperTest extends MallApplicationTests {
+
+    @Test
+    public void findById() {
+    }
+
+    @Test
+    public void queryById() {
+    }
+}
